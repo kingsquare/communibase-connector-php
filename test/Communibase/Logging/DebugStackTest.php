56194a110fcc5dddab33c888bd530aeaa42cb5b4
Aubrey Hewes <aubrey@hewes.org.uk>
2015-06-19 20:29:53 +0200

    added test; though not yet activated as request can not be mocked

---
+++ test/Communibase/Logging/DebugStackTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace Communibase\Test;
+
+/**
+ * Class DebugStackTest
+ *
+ * @package Communibase\Test
+ */
+class DebugStackTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 *
+	 */
+	public function testDebugStackIsFilled() {
+		$this->markTestIncomplete(
+			'currently the logger is within the method which should be mocked for testing purposes; ' .
+			'if the method is mocked then the logger will be empty!'
+		);
+	}
+
+}
